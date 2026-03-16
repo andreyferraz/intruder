ef9eb061921598720ec90018adbd82a283c1fbc4
andreyferraz <andrey.developer@hotmail.com>
2026-03-16 03:10:01 -0300

    first commit

---
+++ src/main/java/com/rifadopunk/intruder/RifaDoPunkApplication.java
@@ -0,0 +1,13 @@
+package com.rifadopunk.intruder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifaDoPunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifaDoPunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rifadopunk/intruder/RifaDoPunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifadopunk.intruder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifaDoPunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
